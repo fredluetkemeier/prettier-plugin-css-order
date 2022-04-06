ae9d38c686c378d01f8025040d1c9e803b82fc64
Fred Luetkemeier <fluetkemeier@midwayusa.com>
2022-04-06 10:40:20 -0500

    Fix sort order

--- src/sorter.js
+++ src/sorter.js
@@ -4,7 +4,7 @@ const { runAsWorker } = require("sync-threads");
 const postcssScss = require("postcss-scss");
 
 runAsWorker(async ({ text, parser }) => {
-  return postcss([cssDeclarationSorter({ order: (a, b) => (a > b ? -1 : 1) })])
+  return postcss([cssDeclarationSorter({ order: (a, b) => (a > b ? 1 : -1) })])
     .process(text, {
       from: undefined,
       syntax: parser === "scss" && postcssScss,
